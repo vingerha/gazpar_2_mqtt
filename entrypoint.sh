484dcf2cae387df13ed2214d05622a39f904510a
Arjan <44190435+vingerha@users.noreply.github.com>
2024-05-01 06:42:06 +0200

    Make sure that core-files are copied to /app, in support of upgrades/updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,41 +9,20 @@ fi
 
 echo "Using '$APP' as APP directory"
 
-if [ ! -f "$APP/database.py" ]; then
-    echo "database.py non existing, copying default to app..."
-    cp /app_temp/database.py "$APP/database.py"
-fi
-if [ ! -f "$APP/gazpar.py" ]; then
-    echo "gazpar.py non existing, copying default to app..."
-    cp /app_temp/gazpar.py "$APP/gazpar.py"
-fi
-if [ ! -f "$APP/gazpar2mqtt.py" ]; then
-    echo "gazpar2mqtt.py non existing, copying default to app..."
-    cp /app_temp/gazpar2mqtt.py "$APP/gazpar2mqtt.py"
-fi
-if [ ! -f "$APP/hass.py" ]; then
-    echo "hass.py non existing, copying default to app..."
-    cp /app_temp/hass.py "$APP/hass.py"
-fi
-if [ ! -f "$APP/influxdb.py" ]; then
-    echo "$APP/influxdb.py non existing, copying default to app..."
-    cp /app_temp/influxdb.py "$APP/influxdb.py"
-fi
-if [ ! -f "$APP/mqtt.py" ]; then
-    echo "mqtt.py non existing, copying default to app..."
-    cp /app_temp/mqtt.py "$APP/mqtt.py"
-fi
+echo "Copying default app/*.py files to app (except param.py)..."
+cp /app_temp/database.py "$APP/database.py"
+cp /app_temp/gazpar.py "$APP/gazpar.py"
+cp /app_temp/gazpar2mqtt.py "$APP/gazpar2mqtt.py"
+cp /app_temp/hass.py "$APP/hass.py"
+cp /app_temp/influxdb.py "$APP/influxdb.py"
+cp /app_temp/mqtt.py "$APP/mqtt.py"
+cp /app_temp/price.py "$APP/price.py"
+cp /app_temp/standalone.py "$APP/standalone.py"
+
 if [ ! -f "$APP/param.py" ]; then
-    echo "param.py non existing, copying default to app..."
+    echo "param.py non existing, copying default to /app..."
     cp /app_temp/param.py "$APP/param.py"
 fi
-if [ ! -f "$APP/price.py" ]; then
-    echo "price.py non existing, copying default to app..."
-    cp /app_temp/price.py "$APP/price.py"
-fi
-if [ ! -f "$APP/standalone.py" ]; then
-    echo "$APP/standalone non existing, copying default to app..."
-    cp /app_temp/standalone.py "$APP/standalone.py"
-fi
+
 
 exec "$@"
