96b898dc357276e1811f1919b924e52b9d7d2a4b
vingerha <44190435+vingerha@users.noreply.github.com>
2024-05-03 18:58:02 +0200

    Tune addon

--- addon/entrypoint.sh
+++ addon/entrypoint.sh
@@ -115,21 +115,6 @@ fi
 APP="/app"
 
 
-echo "Using '$APP' as APP directory"
-
-echo "Copying default app/*.py files to app (except param.py)..."
-cp /app_temp/database.py "$APP/database.py"
-cp /app_temp/gazpar.py "$APP/gazpar.py"
-cp /app_temp/gazpar2mqtt.py "$APP/gazpar2mqtt.py"
-cp /app_temp/hass.py "$APP/hass.py"
-cp /app_temp/influxdb.py "$APP/influxdb.py"
-cp /app_temp/mqtt.py "$APP/mqtt.py"
-cp /app_temp/price.py "$APP/price.py"
-cp /app_temp/standalone.py "$APP/standalone.py"
-
-if [ ! -f "$APP/param.py" ]; then
-    echo "param.py non existing, copying default to /app..."
-    cp /app_temp/param.py "$APP/param.py"
-fi
+# for the addon , no need to copy /app stuff
 
 exec "$@"
